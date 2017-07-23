3c7b552f34d8c73172ee6623a3a19d2ebd201069
ZulioAhuramazda <associate.professor.zulio@gmail.com>
2017-07-22 23:30:45 -0500

    Update bumblebee.php

--- bumblebee.php
+++ bumblebee.php
@@ -1,1 +1,17 @@
-
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    words
+    <p>
+      <?php printf "Hello, world!" ?>
+    </p>
+    <a href="bumblebee.php">Link!</a>
+    <p>
+      <script>'Hi there!'</script>
+    </p>
+  </body>
+</html>
